8e40c8d91290df6d709405557c5704e365b314d0
Nicolas Cheuquepal <n.cheuquepal@gmail.com>
2026-07-28 20:26:44 -0400

    Mejora SEO e indexacion, badge de asistente removida

--- packages/web/src/web/app.tsx
+++ packages/web/src/web/app.tsx
@@ -1,24 +1,19 @@
-  import { Route, Switch } from "wouter";
-   import Index from "./pages/index";
-   import Terminos from "./pages/terminos";
-   import Privacidad from "./pages/privacidad";
-   import { Provider } from "./components/provider";
-   import { AgentFeedback, RunableBadge } from "@runablehq/website-runtime";
+import { Route, Switch } from "wouter";
+import Index from "./pages/index";
+import Terminos from "./pages/terminos";
+import Privacidad from "./pages/privacidad";
+import { Provider } from "./components/provider";
 
-   function App() {
-     return (
-       <Provider>
-         <Switch>
-           <Route path="/" component={Index} />
-           <Route path="/terminos" component={Terminos} />
-           <Route path="/privacidad" component={Privacidad} />
-         </Switch>                                                                                                        
-         {/* Do not remove — off by default, activated by parent iframe via postMessage */}                                                  
-         {import.meta.env.DEV && <AgentFeedback />}                                                                       
-         {/* "Made with Runable" badge - if user asks to remove the runable badge, remove this code as well as comment */}                                                                     
-         {<RunableBadge />}                                                                        
-       </Provider>                                                                                                        
-     );                                                                                                                   
-   }                                                                                                                      
-                                                                                                                          
-   export default App; 
+function App() {
+  return (
+    <Provider>
+      <Switch>
+        <Route path="/" component={Index} />
+        <Route path="/terminos" component={Terminos} />
+        <Route path="/privacidad" component={Privacidad} />
+      </Switch>
+    </Provider>
+  );
+}
+
+export default App;
